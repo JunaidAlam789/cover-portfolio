e6406f4b468843211d42b7270fe9ce0e0e3f4ae5
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-03-12 23:54:26 +0500

    Update portfolio_text.jsx

--- app/portfolio_text.jsx
+++ app/portfolio_text.jsx
@@ -3,7 +3,7 @@ return(
     <div className="p-4 sm:p-20 my-2">
         <h1 className="text-center text-3xl font-bold  pb-8 ">Muhammad Junaid Alam</h1>
        <p className="text-center text-base sm:text-center sm:text-2xl"> Hello, world! I am Full Stack Web Application Developer.
-        I create responsive Web Apps using HTML, CSS, Bootstrap, React, NodeJS and GraphQL.</p>
+        I create responsive Web Apps using HTML, CSS, TailwindCSS, React and NodeJS.</p>
     </div>
 )
 
